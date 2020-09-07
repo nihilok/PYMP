240b5f8dedf510af45f7798253ab9d0fdb2c186b
nihilok <t0k3n8l4ck@gmail.com>
2020-09-07 12:03:25 +0100

    gui-v0.3 - console output

--- pymp.py
+++ pymp.py
@@ -1,70 +1,83 @@
 from __future__ import unicode_literals
 import tkinter as tk
 import youtube_dl
+# import multiprocessing
+import threading
 import time
 
-HEIGHT = 400
-WIDTH = 600
+#def wait function
+def wait(x):
+    time.sleep(x)
+
+# youtube_dl settings:
+def my_hook(d):
+    if d['status'] == 'finished':
+        var_1.set("Download Complete!")
+        wait(3)
+        var_1.set("Welcome to mp3tub3!")
+    elif d['status'] == 'downloading':
+        status = ("Downloading...", d['_percent_str'])
+        var_1.set(status)
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        wait(2)
+        var_1.set("Error! Did you paste the URL correctly?!")
 
 
 ydl_opts = {
+    'progress_hooks': [my_hook],
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '320',
     }],
+    'logger': MyLogger(),
 }
+
+
+# Download funtion:
+def mp3down():
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([paste_field.get()])
 # test url: https://www.youtube.com/watch?v=Jh9PxxggpI4
 
 
-def dl():
+def empty():
     if len(paste_field.get()) <= 0:
-        var_1.set("You forgot to paste the URL")
-    elif len(paste_field.get()) > 0:
-        dling()
+        var_1.set("You forgot to paste the URL!")
+    else:
+        pass
 
 
-def mp3down(paste):
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([paste])
-        finished()
+# threading
+def refresh_threads():
+    t1 = threading.Thread(target=mp3down)
+    t2 = threading.Thread(target=empty)
+    t1.start()
+    t2.start()
 
 
-def dling():
-    var_1.set("Downloading")
+# multiprocessing
+# p1 = multiprocessing.Process(target=mp3down)
+# p2 = multiprocessing.Process(target=empty)
+#
+#
+# def start():
+#     p1.start()
+#     p2.start()
 
 
-def finished():
-    var_1.set("Download complete!")
-
-
-def wait():
-    time.sleep(0.5)
-
-# I'm having trouble here with getting the "Downloading" label to show.
-# I've tried a ton of things and can get it to show from inside the function but it interrupts the actual download.
-
-# def mp3down(paste):
-#     if len(paste) <= 0:
-#         label.config(text="You forgot to paste the URL")
-#     else:
-#         label.config(text="Downloading...")
-#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#         ydl.download([paste])
-#     label.config(text="Download Complete!")
-
-
-# def draw_frame():
-#    middleframe = tk.Frame(root, bg="black")
-#    middleframe.place(relx=0.1, rely=0.45, relwidth=0.8, relheight=0.1)
-
-
-# Trying to print song info:
-# def title(paste):
-#    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#        ydl.extract_info([paste], download=False)
-
+HEIGHT = 400
+WIDTH = 600
 
 root = tk.Tk()
 root.title("mp3tub3 v0.2 (Run inside the directory you want to download to...)")
@@ -82,7 +95,8 @@ middleframe = tk.Frame(root, bg="black")
 middleframe.place(relx=0.1, rely=0.45, relwidth=0.8, relheight=0.1)
 
 var_1 = tk.StringVar()
-var_1.set("Welcome to MP3TUB3!")
+var_1.set("Welcome to mp3tub3!")
+
 label = tk.Label(middleframe,
                  textvariable=var_1,
                  bg="black",
@@ -94,7 +108,8 @@ bottomframe.place(relx=0.1, rely=0.6, relwidth=0.8, relheight=0.3)
 
 download = tk.Button(bottomframe,
                      text="Download MP3",
-                     command=lambda: [dl(), wait(), mp3down(paste_field.get())])
+                     fg="green",
+                     command=lambda: refresh_threads())
 download.place(relx=0.1, rely=0.1, relheight=0.8, relwidth=0.8)
 
 instruction = tk.Label(topframe,
