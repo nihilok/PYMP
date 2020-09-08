6bf158856896956e8a998555fc17314fae4fdab7
nihilok <t0k3n8l4ck@gmail.com>
2020-09-08 10:18:06 +0100

    still working on threading with pyqt

--- pympqt.py
+++ pympqt.py
@@ -2,76 +2,57 @@ import sys
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
 import PyQt5.QtCore as qtc
-import youtube_dl
+from youtube_dl import YoutubeDL
 import time
 
 
-test_url = 'https://www.youtube.com/watch?v=YgGzAKP_HuM'
-
-
 # class MyLogger(qtc.QRunnable):
-#     status = qtc.pyqtSignal(str)
 #     def run(self):
 #         pass
 
-#     def debug(self, msg):
-#         self.status.emit(msg)
-#         # pass
+class MyLogger(object):
 
-#     def warning(self, msg):
-#         pass
+    def debug(self, msg):
+        mw.change_status(msg)
 
-#     def error(self, msg):
-#         self.status.emit(msg)
-#         # pass
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
 
 
 # def my_hook(d):
 #     if d['status'] == 'finished':
 #         mw.change_status("Download complete, now converting...")
 #     elif d['status'] == 'downloading':
-#         mw.statusBar().showMessage("Downloading...", d['_percent_str'])
-# #     pass
+#         mw.change_status("Downloading...", d['_percent_str'])
+
+ydl_opts_mp3 = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    # 'logger': MyLogger(),
+    # 'progress_hooks': [my_hook],
+}
+
+ydl_opts_mp4 = {
+    'format': 'best[ext=mp4]',
+    # 'logger': MyLogger(),
+    # 'progress_hooks': [my_hook],
+}
 
 
 class MainWindow(qtw.QMainWindow):
+    test_url = 'https://www.youtube.com/watch?v=YgGzAKP_HuM'
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle('PYMP')
         self.setFixedSize(300, 175)
-        self.setCentralWidget(MainWidget())
-        menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu('File')
-        file_menu.addAction('Import Bookmarks')
-        file_menu.addSeparator()
-        file_menu.addAction('Quit', self.close)
-        help_menu = menu_bar.addMenu('Help')
-        help_menu.addAction('Help')
-        help_menu.addAction('About')
-        self.statusBar().showMessage('Ready')
-
-        # self.threadpool = qtc.QThreadPool()
-        # print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
-
-        self.show()
-
-    @qtc.pyqtSlot()
-    def change_status(self, msg):
-        self.statusBar().showMessage(msg)
-
-
-class MainWidget(qtw.QWidget):
-    radio = ''
-    status_signal = qtc.pyqtSignal(str)
-
-    def __init__(self):
-        super().__init__()
-        self.setLayout(qtw.QVBoxLayout())
-        self.elements()
-        self.show()
-
-    def elements(self):
         container = qtw.QWidget()
         container.setLayout(qtw.QGridLayout())
 
@@ -83,74 +64,56 @@ class MainWidget(qtw.QWidget):
         self.mp3_radio.toggled.connect(self.FormatSelector)
         self.mp4_radio = qtw.QRadioButton('MP4')
         self.mp4_radio.toggled.connect(self.FormatSelector)
-        self.status_signal.connect(MainWindow.change_status)
+        self.ydl_opts = None
+        container.layout().addWidget(label, 0, 0, 1, 3)
+        container.layout().addWidget(self.paste_field, 1, 0, 1, 5)
+        container.layout().addWidget(self.dld_btn, 3, 0, 1, 5)
+        container.layout().addWidget(self.mp3_radio, 2, 0, 1, 2)
+        container.layout().addWidget(self.mp4_radio, 2, 2, 1, 2)
+        self.setCentralWidget(container)
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('File')
+        file_menu.addAction('Import Bookmarks')
+        file_menu.addSeparator()
+        file_menu.addAction('Quit', self.close)
+        help_menu = menu_bar.addMenu('Help')
+        help_menu.addAction('Help')
+        help_menu.addAction('About')
+        self.statusBar().showMessage('Ready')
+        # self.threadpool = qtc.QThreadPool()
+        # print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+        # worker = self.download
+        # self.threadpool.start(worker)
+        self.show()
 
-        container.layout().addWidget(label, 0,0,1,3)
-        container.layout().addWidget(self.paste_field,1,0,1,5)
-        container.layout().addWidget(self.dld_btn,3,0,1,5)
-        container.layout().addWidget(self.mp3_radio,2,0,1,2)
-        container.layout().addWidget(self.mp4_radio,2,2,1,2)
-        self.layout().addWidget(container)
-
+    def change_status(self, msg):
+        self.statusBar().showMessage(msg)
 
     def FormatSelector(self):
         radioBtn = self.sender()
-
         if radioBtn.isChecked():
             self.radio = radioBtn.text()
-
             if self.radio == 'MP3':
                 mw.change_status('MP3 selected')
-
+                self.ydl_opts = ydl_opts_mp3
             elif self.radio == 'MP4':
                 mw.change_status('MP4 selected')
+                self.ydl_opts = ydl_opts_mp4
 
     def paster(self):
         self.url = self.paste_field.text()
         return self.url
 
-
-
     def download(self):
-        # worker = MyLogger()
-        # mw.threadpool.start(worker)
-        ydl_opts_mp3 = {
-                'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192', 
-                    }],
-                # 'logger': MyLogger(),
-                # 'progress_hooks': [my_hook],
-            }
-
-        ydl_opts_mp4 = {
-                'format': 'best[ext=mp4]',
-                # 'logger': MyLogger(),
-                # 'progress_hooks': [my_hook],
-            }
-
         url = self.paster()
-        print(url)
-
-        if self.radio == 'MP3':
-            with youtube_dl.YoutubeDL(ydl_opts_mp3) as ydl:
-                try:
-                    ydl.download([url])
-                    # mw.statusBar().showMessage('Ready')
-                    mw.change_status('Ready')
-                except:
-                    mw.change_status('Error')
-                    
-        elif self.radio == 'MP4':
-            mw.change_status('Starting Download...')
-            with youtube_dl.YoutubeDL(ydl_opts_mp4) as ydl:
-                try:
-                    ydl.download([url])
-                    mw.change_status('Ready')
-                except:
-                    mw.change_status('Error')
+        # url = self.test_url
+        # print('using test url: ', url)
+        with YoutubeDL(self.ydl_opts) as ydl:
+            try:
+                ydl.download([url])
+                self.change_status('Ready')
+            except:
+                self.change_status('Error')
 
 
 if __name__ == '__main__':
